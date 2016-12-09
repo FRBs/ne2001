09dae6a321e60c74c7cd31f6f283f7181a40ab52
Ben Bar-Or <benbaror@gmail.com>
2016-12-09 13:28:49 -0500

    Added profiling

---
+++ tests/profile/profile_ne2001.py
@@ -0,0 +1,34 @@
+
+import os
+import time
+import cProfile
+import pstats
+
+import numpy as np
+from click.testing import CliRunner
+from numpy.random import rand
+from numpy.random import randint
+
+from ne2001 import density
+from ne2001.cli import main
+
+PARAMS = density.PARAMS
+r_sun = 8.5
+
+
+if __name__ == '__main__':
+    tol = 1e-3
+    ne = density.ElectronDensity(**PARAMS)
+    xyz = np.array([-3.4153607E-02,   7.521969,      0.2080137])
+    DM = 23.98557
+    start = time.time()
+    DMc = ne.DM(xyz)
+    t1 = time.time() - start
+    start = time.time()
+    DMc = ne.DM(xyz)
+    t2 = time.time() - start
+#    assert abs(DMc - DM)/DM < tol
+    cProfile.run('ne.DM(xyz)', 'restats')
+    print(t1,t2)
+    p = pstats.Stats('restats')
+    p.strip_dirs().sort_stats('cumulative').print_stats(50)
